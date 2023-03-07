e223881662cc95c585e45570dd121c06f5f17cc6
Paul Cleary <cleary.paul@protonmail.com>
2023-03-06 19:22:02 -0800

    refactor: Fixing and testing gallery with ImageList

--- src/components/Projects/ProjectDetails/ProjectDetails.js
+++ src/components/Projects/ProjectDetails/ProjectDetails.js
@@ -1,180 +1,120 @@
-import * as React from 'react';
-import { ImageList, ImageListItem } from '@mui/material';
+// import * as React from 'react';
+// import { ImageList, ImageListItem } from '@mui/material';
 
-// import { ThemeContext } from '../../contexts/ThemeContext';
-// import { projectsPhotoData } from '../../data/projectsPhotoData'
-// import { HiArrowRight } from "react-icons/hi";
+// // import { ThemeContext } from '../../contexts/ThemeContext';
+// // import { projectsPhotoData } from '../../data/projectsPhotoData'
+// // import { HiArrowRight } from "react-icons/hi";
 
-import "./ProjectDetails.css";
+// import "./ProjectDetails.css";
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
+// function ProjectDetailsPage() {
+//   const { theme } = useContext(ThemeContext);
 
-export default function ProjectDetailsPage() {
-  return (
-    <ImageList
-      sx={{ width: 500, height: 450 }}
-      variant="quilted"
-      cols={4}
-      rowHeight={121}
-    >
-      {itemData.map((item) => (
-        <ImageListItem
-          key={item.img}
-          cols={item.cols || 1}
-          rows={item.rows || 1}
-        >
-          <img
-            {...srcset(item.img, 121, item.rows, item.cols)}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
-}
+//   const useStyles = makeStyles(() => ({
+//     viewAllBtn: {
+//       color: theme.tertiary,
+//       backgroundColor: theme.primary,
+//       transition: "color 0.2s",
+//       "&:hover": {
+//         color: theme.secondary,
+//         backgroundColor: theme.primary,
+//       },
+//     },
+//     viewArr: {
+//       color: theme.tertiary,
+//       backgroundColor: theme.secondary70,
+//       width: "40px",
+//       height: "40px",
+//       padding: "0.5rem",
+//       fontSize: "1.05rem",
+//       borderRadius: "50%",
+//       cursor: "pointer",
+//       transition: "background-color 0.2s",
+//       "&:hover": {
+//         color: theme.tertiary,
+//         backgroundColor: theme.secondary,
+//       },
+//     },
+//   }));
 
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    cols: 2,
-  },
-];
-
-// function ProjectDetails() {
-
-//     const { theme } = useContext(ThemeContext);
-
-//     const useStyles = makeStyles(() => ({
-//         viewAllBtn : {
-//             color: theme.tertiary,
-//             backgroundColor: theme.primary,
-//             transition: 'color 0.2s',
-//             "&:hover": {
-//                 color: theme.secondary,
-//                 backgroundColor: theme.primary,
-//             }
-//         },
-//         viewArr : {
-//             color: theme.tertiary,
-//             backgroundColor: theme.secondary70,
-//             width: '40px',
-//             height: '40px',
-//             padding: '0.5rem',
-//             fontSize: '1.05rem',
-//             borderRadius: '50%',
-//             cursor: 'pointer',
-//             transition: 'background-color 0.2s',
-//             "&:hover": {
-//                 color: theme.tertiary,
-//                 backgroundColor: theme.secondary,
-//             }
-//         },
-//     }));
-
-//     const classes = useStyles();
-
-//     return (
-//         <>
-//             {projectsPhotoData.length > 0 && (
-//                 <div className="projects" id="projects" style={{backgroundColor: theme.secondary}}>
-//                     <div className="projects--header">
-//                         <h1 style={{color: theme.primary}}>Photo Projects</h1>
-//                     </div>
-//                     <div className="projects--body">
-//                         <div className="projects--bodyContainer">
-//                             {projectsPhotoData.slice(0, 3).map(project => (
-//                                 <SingleProject
-//                                     theme={theme}
-//                                     key={project.id}
-//                                     id={project.id}
-//                                     name={project.projectName}
-//                                     desc={project.projectDesc}
-//                                     tags={project.tags}
-//                                     code={project.code}
-//                                     seeMore={project.seeMore}
-//                                     image={project.image}
-//                                 />
-//                             ))}
-//                         </div>
-
-//                         {projectsPhotoData.length > 3 && (
-//                             <div className="projects--viewAll">
-//                                 <Link to="/projects">
-//                                     <button className={classes.viewAllBtn}>
-//                                         View All
-//                                         <HiArrowRight className={classes.viewArr} />
-//                                     </button>
-//                                 </Link>
-//                             </div>
-//                         )}
-//                     </div>
-//                 </div>
-//             )}
-
-//         </>
-//     )
+//   const classes = useStyles();
 // }
+//   export function StandardImageList() {
+//     return (
+//       <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+//         {itemData.map((item) => (
+//           <ImageListItem key={item.img}>
+//             <img
+//               src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+//               srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+//               alt={item.title}
+//               loading="lazy"
+//             />
+//           </ImageListItem>
+//         ))}
+//       </ImageList>
+//     );
+//   }
 
-// export default Projects
+//   const itemData = [
+//   {
+//     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+//     title: "Breakfast",
+//     rows: 2,
+//     cols: 2,
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+//     title: "Burger",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+//     title: "Camera",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+//     title: "Coffee",
+//     cols: 2,
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+//     title: "Hats",
+//     cols: 2,
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+//     title: "Honey",
+//     author: "@arwinneil",
+//     rows: 2,
+//     cols: 2,
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+//     title: "Basketball",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+//     title: "Fern",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+//     title: "Mushrooms",
+//     rows: 2,
+//     cols: 2,
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+//     title: "Tomato basil",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+//     title: "Sea star",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+//     title: "Bike",
+//     cols: 2,
+//   },
+// ];
+
+// export default ProjectDetailsPage;
--- src/pages/Project/ProjectDetailsPage.js
+++ src/pages/Project/ProjectDetailsPage.js
@@ -1,100 +1,165 @@
-import * as React from "react";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import React, { useContext, useState } from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { AiOutlineHome } from "react-icons/ai";
+import { ImageList, ImageListItem } from "@mui/material";
 
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    cols: 2,
-  },
-];
+import "./ProjectDetailsPage.css";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { headerData } from "../../data/headerData";
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
+function ProjectDetailsPage() {
+  const [search, setSearch] = useState("");
+  const { theme } = useContext(ThemeContext);
 
-const ProjectDetailsPage = () => {
-  return (
-    <ImageList
-      sx={{ width: 500, height: 450 }}
-      variant="quilted"
-      cols={4}
-      rowHeight={121}
-    >
-      {itemData.map((item) => (
-        <ImageListItem
-          key={item.img}
-          cols={item.cols || 1}
-          rows={item.rows || 1}
+  const useStyles = makeStyles((t) => ({
+    search: {
+      color: theme.tertiary,
+      width: "40%",
+      height: "2.75rem",
+      outline: "none",
+      border: "none",
+      borderRadius: "20px",
+      padding: "0.95rem 1rem",
+      fontFamily: "'Noto Sans TC', sans-serif",
+      fontWeight: 500,
+      fontSize: "0.9rem",
+      backgroundColor: theme.secondary,
+      boxShadow:
+        theme.type === "dark"
+          ? "inset 3px 3px 6px #ffffff10, inset -3px -3px 6px #00000060"
+          : "inset 3px 3px 6px #ffffffbd, inset -3px -3px 6px #00000030",
+      "&::placeholder": {
+        color: theme.tertiary80,
+      },
+      [t.breakpoints.down("sm")]: {
+        width: "350px",
+      },
+    },
+    home: {
+      color: theme.secondary,
+      position: "absolute",
+      top: 25,
+      left: 25,
+      padding: "7px",
+      borderRadius: "50%",
+      boxSizing: "content-box",
+      fontSize: "2rem",
+      cursor: "pointer",
+      boxShadow:
+        theme.type === "dark"
+          ? "3px 3px 6px #ffffff40, -3px -3px 6px #00000050"
+          : "3px 3px 6px #ffffff40, -3px -3px 6px #00000050",
+      transition: "all 0.3s ease-in-out",
+      "&:hover": {
+        color: theme.tertiary,
+        transform: "scale(1.1)",
+      },
+      [t.breakpoints.down("sm")]: {
+        fontSize: "1.8rem",
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
+  const itemData = [
+    {
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Breakfast",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Burger",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Camera",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Coffee",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+      title: "Hats",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+      title: "Honey",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+      title: "Mushrooms",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+      title: "Tomato basil",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+      title: "Sea star",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+      title: "Bike",
+    },
+  ];
+
+    return (
+      <div className="projectPage" style={{ backgroundColor: theme.secondary }}>
+        <Helmet>
+          <title>{headerData.name} | Photo Projects</title>
+        </Helmet>
+        <div
+          className="projectPage-header"
+          style={{ backgroundColor: theme.primary }}
         >
-          <img
-            {...srcset(item.img, 121, item.rows, item.cols)}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
-};
+          <Link to="/">
+            <AiOutlineHome className={classes.home} />
+          </Link>
+          <h1 style={{ color: theme.secondary }}>Photo Projects</h1>
+        </div>
+        <div className="projectPage-container">
+          <div className="projectPage-search">
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search project..."
+              className={classes.search}
+            />
+          </div>
+          <div className="project-container">
+            <ImageList
+              sx={{ width: 900, height: 450 }}
+              cols={3}
+              rowHeight={300}
+            >
+              {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                  <img
+                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.title}
+                    loading="lazy"
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 export default ProjectDetailsPage;
