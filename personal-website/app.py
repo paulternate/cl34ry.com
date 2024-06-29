52af3f0e1931800a9b3c9d3c4e72005655ac07ce
paulternate <cleary.paul@protonmail.com>
2024-06-28 23:33:52 -0700

    making progress

--- personal-website/app.py
+++ personal-website/app.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask, render_template
+from livereload import Server
 
 def create_app():
     app = Flask(__name__)
+
+    # Disable template caching
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['DEBUG'] = True
 
     @app.route('/')
     def index():
@@ -19,4 +25,13 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+
+    # Set environment variables
+    os.environ['FLASK_ENV'] = 'development'
+
+    # Set up livereload server
+    server = Server(app.wsgi_app)
+    server.watch('templates/*.html')
+    server.watch('static/css/*.css')
+    server.watch('static/js/*.js')
+    server.serve(port=5000, host='127.0.0.1', debug=True)
