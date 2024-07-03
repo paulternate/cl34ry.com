3f889af9e5cbd0b3fcaca567c2317e1123292ebd
paulternate <cleary.paul@protonmail.com>
2024-07-03 07:15:00 -0700

    working as expected

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import Flask, render_template
 from livereload import Server
 from flask_frozen import Freezer
 from werkzeug.serving import run_simple
+import shutil
 
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True  # Enable debug mode
     app.config['FREEZER_DESTINATION'] = 'docs'  # Set the destination directory for Flask Freezer
+    app.config['FREEZER_BASE_URL'] = 'https://cl34ry.com/'  # Set the base URL for Flask Freezer
 
     @app.route('/')
     def index():
@@ -17,14 +19,6 @@ def create_app():
     @app.route('/resume.html')
     def resume():
         return render_template('resume.html')
-
-    # @app.route('/hobbies.html')
-    # def hobbies():
-    #     images_places = [f for f in os.listdir(os.path.join(app.static_folder, 'images/photos/places')) if os.path.isfile(os.path.join(app.static_folder, 'images/photos/places', f))]
-    #     images_people = [f for f in os.listdir(os.path.join(app.static_folder, 'images/photos/people')) if os.path.isfile(os.path.join(app.static_folder, 'images/photos/people', f))]
-    #     print('Images Places:', images_places)  # Debug print
-    #     print('Images People:', images_people)  # Debug print
-    #     return render_template('hobbies.html', images_places=images_places, images_people=images_people)
 
     return app
 
@@ -41,6 +35,19 @@ def url_generators():
         print(f"Yielding URL: {url}")  # Debug print
         yield url
 
+def copy_files():
+    # Ensure the CNAME & .nojekyll are present in the docs directory
+    files_to_copy = ['CNAME', '.nojekyll']
+    dest_dir = 'docs'
+
+    for file_name in files_to_copy:
+        source_file = file_name
+        dest_file = os.path.join(dest_dir, file_name)
+        if os.path.exists(source_file):
+            shutil.copy(source_file, dest_file)
+            print(f"Copied {source_file} to {dest_file}")
+        else:
+            print(f"{source_file} does not exist.")
 
 if __name__ == '__main__':
     # Set environment variables
@@ -50,13 +57,6 @@ if __name__ == '__main__':
         command = sys.argv[1]
 
         if command == 'run':
-            # Set up livereload server for development
-            # server = Server(app.wsgi_app)
-            # server.watch('templates/*.html')
-            # server.watch('static/css/*.css')
-            # server.watch('static/js/*.js')
-            # server.serve(port=5000, host='127.0.0.1', debug=True)
-
             run_simple('localhost', 5000, app, use_reloader=True, extra_files=[''])
         
         elif command == 'freeze':
@@ -66,6 +66,7 @@ if __name__ == '__main__':
                 for url in freezer.freeze_yield():
                     print(f"Freezing URL: {url.url}")
                 freezer.freeze()
+                copy_files()  # Copy the CNAME & .nojekyll files after freezing
             except AssertionError as e:
                 print(f"AssertionError: {e}")
                 print("Check your URL paths and ensure they start with '/'.")
